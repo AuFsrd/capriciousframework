6f216aa447280170b8c3174cbf2a0982237d329f
Aurelien Fossard <aurelien.fossard2023@campus-eni.fr>
2023-08-25 14:09:07 +0200

    Minor changes

--- src/main/java/com/capriciousframework/services/CapriciousService.java
+++ src/main/java/com/capriciousframework/services/CapriciousService.java
@@ -9,14 +9,14 @@ import java.util.List;
  * Le service capricieux fournit des méthodes CRUD basiques. Pour l'utiliser, il suffit de faire hériter une classe
  * de ce service en lui passant le type de l'entité souhaitée.
  * Attention, il est capricieux. Si vous lui en demandez trop, il risque de ne pas se comporter comme prévu.
- * Demandez poliment et penser à dire merci !
+ * Demandez poliment et pensez à dire merci !
  * @param <T>
  */
 public interface CapriciousService<T extends BaseEntity> {
 
     /**
      * Permet de savoir si le service est d'humeur à retourner des objets.
-     * @return false s'il est trop frustré pour coopérer. Pensez à la complimenter !
+     * @return false s'il est trop frustré pour coopérer. Pensez à le complimenter !
      */
     public Boolean isInTheMood();
 
@@ -25,7 +25,7 @@ public interface CapriciousService<T extends BaseEntity> {
      * et apaise ses frustrations.
      * @param compliment Ecrivez un compliment.
      */
-    public void compliment(String compliment);
+    public String compliment(String compliment);
 
     /**
      * Il est conseillé de remercier le service après chaque utilisation.
@@ -40,17 +40,14 @@ public interface CapriciousService<T extends BaseEntity> {
     public MethodProvider<T> couldyou();
 
     /**
-     * Cette classe interne fournit toutes les méthodes CRUD du service. Ces méthodes ne sont accessibles en
-     * demandant poliment. Attention, le service se souviendra de chaque demande que vous lui faites, pensez
-     * donc à le remercier et le complimenter régulièrement.
+     * Cette classe interne fournit toutes les méthodes CRUD du service. Ces méthodes ne sont accessibles qu'en
+     * demandant poliment via la méthode couldyou(). Attention, le service se souviendra de chaque demande que
+     * vous lui faites, pensez donc à le remercier et le complimenter régulièrement.
      */
     interface MethodProvider<T extends BaseEntity> {
         public Maybe<T> getById(Integer id);
-
         public Maybe<List<T>> getAll();
-
         public Maybe<T> save(T t);
-
         public void delete(T t);
     }
 }
